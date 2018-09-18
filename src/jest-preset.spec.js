da527b158c42f06fa7b28ba166bf666bf90435e9
Kevin Ruff <kevin.ruff@immonet.de>
2018-09-18 08:17:31 +0200

    BUGFIX: Solve tests.

--- src/jest-preset.spec.js
+++ src/jest-preset.spec.js
@@ -1,7 +1,15 @@
-const config = require('../jest-preset');
+const {resolve} = require('path');
+const {existsSync, readFileSync} = require('fs');
 
 describe('json-preset', () => {
+  const configPath = resolve(__dirname, '../jest-preset.json');
+
+  it('should be existing', () => {
+    expect(existsSync(configPath)).toBe(true);
+  });
+
   it('should be valid json', () => {
+    const config = readFileSync(configPath);
     expect(JSON.parse(config)).toBeDefined();
   });
 });
